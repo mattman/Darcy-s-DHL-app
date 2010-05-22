7076349167699406e71f0abc6e1f9e511245555b
Matt Didcoe <matt@didcoe.id.au>
2010-05-22 15:36:01 +0800

    maps JS

--- public/javascripts/whereru.js
+++ public/javascripts/whereru.js
@@ -9,16 +9,14 @@ function pathmap(coords) {
   };
 
   var pmap = new google.maps.Map(document.getElementById("pathmap"), opts);
+
   var pos = [];
+
   for ( var i=0, len=coords.length; i<len; ++i ){
-	  a = coords[i].split(",")
-	  coords[i]
+	  a = coords[i].split(",");
+	  coords[i] = new google.maps.LatLng(a[0],a[1]);
 	}
-    new google.maps.LatLng(37.772323, -122.214897),
-    new google.maps.LatLng(21.291982, -157.821856),
-    new google.maps.LatLng(-18.142599, 178.431),
-    new google.maps.LatLng(-27.46758, 153.027892)
-  ];
+  
   var map = new google.maps.Polyline({
     path: pos,
     strokeColor: "#FF0000",
